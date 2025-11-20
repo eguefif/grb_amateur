a06e3467175f2879864b3174d0f348300a6c3233
Emmanuel Guefif <eguefif@fastmail.com>
2025-11-20 20:49:36 +0100

    backend: add route to get last 5 events

--- web_backend/grb_alerts/routes.py
+++ web_backend/grb_alerts/routes.py
@@ -10,3 +10,10 @@ router = APIRouter(prefix = "/events")
 async def add_event(session: SessionDep, alert: GRBAlert) -> GRBAlert:
     alert = service.add_alert(session, alert)
     return alert
+
+@router.get("/last_events")
+async def last_events(session: SessionDep) -> list[GRBAlert]:
+    alerts = service.read_events(session, offset = 0, limit = 5)
+    if not alerts:
+        raise HTTPException(status_code=404, detail='No events yet')
+    return alerts
--- web_backend/grb_alerts/service.py
+++ web_backend/grb_alerts/service.py
@@ -1,3 +1,7 @@
+from typing import Annotated
+from fastapi import Query
+from sqlmodel import select
+
 from db import SessionDep
 
 from grb_alerts.models import GRBAlert
@@ -8,3 +12,12 @@ def add_alert(session: SessionDep, alert: GRBAlert) -> GRBAlert:
     session.commit()
     session.refresh(alert)
     return alert
+
+def read_events(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+            ) -> list[GRBAlert]:
+    alerts = session.exec(select(GRBAlert).offset(offset).limit(limit)).all()
+    return alerts
+
