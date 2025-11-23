a16ba1e5f530153bf2426ad4cc595990cff345eb
Emmanuel Guefif <eguefif@fastmail.com>
2025-11-23 20:22:41 +0100

    backend: refactor user table and models

--- web_backend/users/models.py
+++ web_backend/users/models.py
@@ -1,9 +1,21 @@
 from sqlmodel import Field, SQLModel
 
+class UserBase(SQLModel):
+    email: str = Field(index=True, unique=True)
+    full_name: str
 
-class User(SQLModel, table=True):
+
+class User(UserBase, table=True):
     __tablename__ = "users"
 
     id: int | None = Field(default=None, primary_key=True)
-    email: str = Field(index=True, unique=True)
     email_confirmed: bool
+    hashed_password: str
+
+class UserIn(UserBase):
+    password: str
+    password_confirmation: str
+
+
+class UserOut(UserBase):
+    id: int
