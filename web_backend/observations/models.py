b6a90f1e80170cdb0a3e32c1519479a31778a948
Emmanuel Guefif <eguefif@fastmail.com>
2025-11-23 15:32:16 +0100

    backend: add observation image table

--- web_backend/observations/models.py
+++ web_backend/observations/models.py
@@ -1,5 +1,5 @@
 from sqlmodel import SQLModel, Field, Relationship
-from grb_alerts.models import GRBAlert, GRBPosition
+from grb_alerts.models import GRBAlert
 
 
 class Observation(SQLModel, table=True):
@@ -17,3 +17,15 @@ class Observation(SQLModel, table=True):
 
     alert_id: int = Field(default=None, foreign_key="grb_alerts.id")
     alert: GRBAlert = Relationship(back_populates="observations")
+
+    observation_image: "ObservationImage" = Relationship(back_populates="observation")
+
+
+class ObservationImage(SQLModel, table=True):
+    __tablename__ = "observation_images"
+
+    id: int | None = Field(default=None, primary_key=True)
+    path: str
+
+    observation_id: int = Field(default=None, foreign_key="observations.id")
+    observation: Observation = Relationship(back_populates="observation_image")
